89d5b5bbae0aa2f59871ac305b79719fdf67556a
Danial <d.khorasanizadeh@ec.iut.ac.ir>
2024-07-18 14:59:42 +0330

    Update Docusaurus and Change CodeStar Docs path

---
+++ docusaurus.config.ts
@@ -0,0 +1,126 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "StarAcademy",
+  tagline: "کنارتیم تا بدرخشی!",
+  favicon: "img/favicon.ico",
+  url: "https://star-academy.github.io/",
+  baseUrl: "/codestar-documents/",
+  organizationName: "Star-Academy",
+  projectName: "codestar-documents",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  i18n: {
+    defaultLocale: "fa",
+    locales: ["fa"],
+    localeConfigs: {
+      fa: {
+        direction: "rtl",
+        htmlLang: "fa-IR",
+      },
+    },
+  },
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          editUrl:
+            "https://github.com/Star-Academy/codestar-documents/tree/master/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'codestar',
+        path: 'CodeStar',
+        routeBasePath: 'codestar',
+        sidebarPath: './sidebarsCodeStar.ts',
+      },
+    ],
+  ],
+  themeConfig: {
+    navbar: {
+      title: "آکادمی ستاره",
+      logo: {
+        alt: "آکادمی ستاره",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "dropdown",
+          position: "left",
+          label: "مستندات",
+          items: [
+            {
+              type: "doc",
+              docId: "general",
+              label: "عمومی",
+            },
+            {
+              type: "doc",
+              docId: "frontend",
+              label: "فرانت‌اند",
+            },
+            {
+              type: "doc",
+              docId: "software-engineering",
+              label: "بک‌اند",
+            },
+            {
+              type: "doc",
+              docId: "soft-skills",
+              label: "مهارت‌های نرم",
+            },
+            {
+              type: "doc",
+              docId: "project",
+              label: "پروژه",
+            },
+          ],
+        },
+        {
+          type: "doc",
+          docId: "onboarding",
+          label: "CodeStar",
+          position: 'left',
+          docsPluginId: 'codestar',
+        },
+        {
+            href: 'https://www.aparat.com/Code_star/',
+            label: 'Aparat',
+            position: 'right',
+        },
+        {
+            href: 'https://github.com/Star-Academy/',
+            label: 'GitHub',
+            position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [],
+      copyright: `آکادمی ستاره - شرکت فناوری اطلاعات و ارتباطات مهیمن © ${new Date().toLocaleDateString("fa-IR").split('/')[0]} `,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+};
+
+export default config;
