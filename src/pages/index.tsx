607ee43d324651bfbcec80486f65be1297920f3b
Danial Khorasanizadeh <36936076+dkhorasanizadeh@users.noreply.github.com>
2024-07-18 15:09:30 +0330

    Update Docusaurus and Change CodeStar Docs path (#131)
    * Update Docusaurus and Change CodeStar Docs path
    
    * Add condition to only deploy when push is on master

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,40 +3,33 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-
+import Lottie from 'lottie-react';
+import animationData from '/animations/documentation.lottie.json';
 import styles from './index.module.css';
 
-export default function Home(): JSX.Element {
+export default function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
-
     return (
         <Layout>
-            <header className={clsx(styles.heroBanner)}>
+            <main className={clsx(styles.mainContainer)}>
                 <div className="container">
                     <h1 className="hero__title">{siteConfig.title}</h1>
                     <p className="hero__subtitle">{siteConfig.tagline}</p>
                     <div className={styles.buttons}>
-                        <Link className="button button--secondary button--lg" to="/docs/intro">
-                            مقدمات
-                        </Link>
-                        <Link className="button button--secondary button--lg" to="/docs/frontend">
-                            فرانت‌اند
-                        </Link>
-                        <Link className="button button--secondary button--lg" to="/docs/software-engineering">
-                            مهندسی نرم‌افزار
-                        </Link>
-                        <Link className="button button--secondary button--lg" to="/docs/general">
-                            عمومی
-                        </Link>
-                        <Link className="button button--secondary button--lg" to="/docs/project">
-                            پروژه
-                        </Link>
-                        <Link className="button button--secondary button--lg" to="/docs/roadmaps">
-                            مسیرهای یادگیری
-                        </Link>
+                            <Link
+                                className="button button--secondary button--lg"
+                                to="codestar/onboarding"
+                                >
+                                CodeStar
+                            </Link>
                     </div>
                 </div>
-            </header>
+                <Lottie
+                    animationData={animationData}
+                    loop={true}
+                />
+
+            </main>
         </Layout>
     );
 }
