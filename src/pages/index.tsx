1e82697ef762e042f347a13e7d1e8c1ff3f9ae59
Mahdi Mazaheri <117896016+mazaheriMahdi@users.noreply.github.com>
2025-06-08 23:05:16 +0330

    Update deploy.yml (#174)
    fixed pipline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,12 +16,12 @@ export default function HomepageHeader() {
                     <h1 className="hero__title">{siteConfig.title}</h1>
                     <p className="hero__subtitle">{siteConfig.tagline}</p>
                     <div className={styles.buttons}>
-                            <Link
-                                className="button button--secondary button--lg"
-                                to="codestar/onboarding"
-                                >
-                                CodeStar
-                            </Link>
+                        <Link
+                            className="button button--secondary button--lg"
+                            to="codestar/about"
+                        >
+                            CodeStar
+                        </Link>
                     </div>
                 </div>
                 <Lottie
